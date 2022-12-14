53993aab2da3ffbc3a987de78782f71ebfb98574
matrix <zhangxw0413@gmail.com>
2022-12-14 10:45:45 +0000

    update

--- t5x_scripts/run.sh
+++ t5x_scripts/run.sh
@@ -3,12 +3,12 @@ PWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 #SIZE="base"
 #SIZE="xl"
 SIZE="xxl"
-PROJECT_DIR=$PWD/flan_mt5_$SIZE
+PROJECT_DIR=$PWD/flan_mt5_${SIZE}_chat
 #PROJECT_DIR=$PWD/
 T5X_DIR=$PWD/t5x/  # directory where the T5X repo is cloned.
 export PYTHONPATH=${PROJECT_DIR}:${T5X_DIR}
 # Model dir to save logs, ckpts, etc. in "gs://model_dir" format.
-MODEL_DIR="gs://clueai/models/zxw/chat_mt5_t5x_${SIZE}"
+MODEL_DIR="gs://clueai/models/zxw/mt5_t5x_${SIZE}_chat"
 
 python3 ${T5X_DIR}/t5x/train.py \
   --gin_search_paths=${PROJECT_DIR} \
